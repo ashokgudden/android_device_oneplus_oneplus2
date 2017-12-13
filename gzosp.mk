3dae3dcc3429adde93a0f602b44117c78adb8cd5
Ashok Soni <ashok.soni@gmail.com>
2017-12-13 08:24:06 +0000

    Few changes

--- gzosp.mk
+++ gzosp.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
 # Inherit some common GZOSP stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+#$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := gzosp_oneplus2
 PRODUCT_DEVICE := oneplus2
